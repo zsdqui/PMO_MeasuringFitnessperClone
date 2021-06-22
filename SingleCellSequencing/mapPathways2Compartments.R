60ece1d37fc4b0110d6f94932e92cc197d0760ac
noemiandor <noemi.andor@gmail.com>
2021-06-22 13:59:27 -0400

    restructured pathway analysis and removed unrelated code

--- SingleCellSequencing/mapPathways2Compartments.R
+++ SingleCellSequencing/mapPathways2Compartments.R
@@ -41,14 +41,6 @@ rgl::movie3d(
 )
 rgl.close()
 
-path2locmap<-read.table(pathwayMapFile, header = FALSE, sep = "\t", dec = ".", comment.char="", quote="", check.names = F, stringsAsFactors = F)
-# Here we replace all compartment names in the path2locmap with their counterpart in coords column names
-path2locmap$V3 <- sapply(strsplit(path2locmap$V3, "[", fixed=TRUE), function(x) (x)[2])
-path2locmap$V3 <- gsub("]", "", path2locmap$V3)
-aliasMap = list("endoplasmic reticulum membrane"="endoplasmic reticulum", "endoplasmic reticulum lumen" = "endoplasmic reticulum", "Golgi membrane" = "Gogli apparatus", "Golgi-associated vesicle lumen" = "Gogli apparatus", "Golgi-associated vesicle membrane" = "Gogli apparatus", "Golgi lumen" = "Gogli apparatus", "trans-Golgi network membrane" = "Gogli apparatus", "nucleoplasm" = "nucleus", "nucleolus" = "nucleus", "nuclear envelope" = "nucleus", "mitochondrial inner membrane" = "mitochondrion", "mitochondrial outer membrane" = "mitochondrion", "mitochondrial intermembrane space" = "mitochondrion", "mitochondrial matrix" = "mitochondrion", "endosome lumen" = "endosome", "endosome membrane" = "endosome", "late endosome lumen" = "endosome", "late endosome membrane" = "endosome", "lysomal lumen" = "lysosome", "lysomal membrane" = "lysosome", "peroxisomal matrix" = "peroxisome", "peroxisomal membrane" = "peroxisome")
-for(x in names(aliasMap)){
-  path2locmap$V3 = str_replace_all(path2locmap$V3, x, aliasMap[[x]])
-}
 
 # Expression profiles of all detected genes for clone 9 in the cell line.
 cID = 9
@@ -70,6 +62,15 @@ if(file.exists(pqFile)){
 ccState = sapply(colnames(pq), function(x) cloneid::getState(x,whichP = "TranscriptomePerspective"))
 
 
+## Pathway information:
+path2locmap<-read.table(pathwayMapFile, header = FALSE, sep = "\t", dec = ".", comment.char="", quote="", check.names = F, stringsAsFactors = F)
+# Here we replace all compartment names in the path2locmap with their counterpart in coords column names
+path2locmap$V3 <- sapply(strsplit(path2locmap$V3, "[", fixed=TRUE), function(x) (x)[2])
+path2locmap$V3 <- gsub("]", "", path2locmap$V3)
+aliasMap = list("endoplasmic reticulum membrane"="endoplasmic reticulum", "endoplasmic reticulum lumen" = "endoplasmic reticulum", "Golgi membrane" = "Gogli apparatus", "Golgi-associated vesicle lumen" = "Gogli apparatus", "Golgi-associated vesicle membrane" = "Gogli apparatus", "Golgi lumen" = "Gogli apparatus", "trans-Golgi network membrane" = "Gogli apparatus", "nucleoplasm" = "nucleus", "nucleolus" = "nucleus", "nuclear envelope" = "nucleus", "mitochondrial inner membrane" = "mitochondrion", "mitochondrial outer membrane" = "mitochondrion", "mitochondrial intermembrane space" = "mitochondrion", "mitochondrial matrix" = "mitochondrion", "endosome lumen" = "endosome", "endosome membrane" = "endosome", "late endosome lumen" = "endosome", "late endosome membrane" = "endosome", "lysomal lumen" = "lysosome", "lysomal membrane" = "lysosome", "peroxisomal matrix" = "peroxisome", "peroxisomal membrane" = "peroxisome")
+for(x in names(aliasMap)){
+  path2locmap$V3 = str_replace_all(path2locmap$V3, x, aliasMap[[x]])
+}
 ## Exclude pathways that are active in undefined locations for now. @TODO: map all pathways later
 path2locmap = path2locmap[path2locmap$V3 %in% colnames(coord)[apply(coord!=0,2,any)],]
 ## Exclude pathways that are in endosome or peroxisome: we did not set their coordinates. @TODO later
@@ -185,10 +186,12 @@ for (cellName in list.dirs(OUTD, full.names = F)){
 }
 
 
+
+
 # f=`ls`
-# for x in $f; do 
-#   echo $x;  
-#   tar -czvf $x.tar.gz $x; 
+# for x in $f; do
+#   echo $x;
+#   tar -czvf $x.tar.gz $x;
 # done
 
 
@@ -206,151 +209,12 @@ for (cellName in list.dirs(OUTD, full.names = F)){
 # dev.off()
 
 
-# align VAE output inside compartment
-coord_nucl = coord[coord$nucleus==1,]
-## Clone_0.0027047_ID107807
-vae = read.csv(paste0(ROOTD,"RNAsequencing/A02_210128_VAEoutput/identities_latentSpace3D.csv"))
-vae=vae[vae$Location=="nucleus",]
-vae=vae[!duplicated(vae),]
-scatterplot3d::scatterplot3d(vae$x, vae$y, vae$z, pch=20)
-alignPathways2Compartments(coord_nucl[,c("x","y","z")], vae[,c("x","y","z")])
-
-
-##########################################################################
-### Do RNA-seq distributions recapitulate those derived from imaging ? ###
-##########################################################################
-gatherNucleusCytoExpression <- function(x, path2locmap, q1=0.005, q2=0.995){
-  x=x+min(x);
-  path2locmap$Location2 = path2locmap$Location
-  path2locmap$Location2[path2locmap$Location %in% c("endoplasmic reticulum","mitochondrion" ) ] = "cytosol" 
-  
-  init = as.matrix(x[1,,drop=F])
-  cum = list(cytosol = init, nucleus = init)
-  for(p in intersect(rownames(x), path2locmap$pathwayname) ){
-    ii = which(path2locmap$pathwayname == p);
-    loi = intersect(path2locmap$Location2[ii], names(cum))
-    if(!isempty(loi)){
-      for(l in loi){
-        cum[[l]] = cum[[l]]+x[p,]
-      }
-    }
-  }
-  # for(l in names(cum)){
-  #   cum[[l]] = cum[[l]] - quantile(cum[[l]],q1)
-  #   cum[[l]] = cum[[l]]/quantile(cum[[l]],q2)
-  # }
-  return(cum)
-}
-
-
-hin = list()
-## Imaging data
-f=c("~/QuPath/output/NUGC-4A_A8_seedTPd6.txt", "~/QuPath/output/SNU-16C_A7_seedTPd4.txt")
-imgdat = lapply(f, function(x) read.table(x,sep="\t", check.names = F, stringsAsFactors = F, header=T));
-# names(imgdat) = sapply(f, function(x) fileparts(x)$name)
-names(imgdat) = c("NUGC-4","SNU-16")
-sapply(imgdat, dim)
-for(y in names(imgdat)){
-  x = imgdat[[y]]
-  x = x[x$`Cell: Area`>=40 & x$`Cell: Area`<=600,]
-  x = x[x$`Nucleus: Area`>=10,]
-  imgdat[[y]] = x
-}
-
-what = "Nucleus/Cell area ratio"
-# what = "Nucleus: Area"
-# what = "Cell: Area"
-# what = "Nucleus: Perimeter"
-# what = "Cell: Perimeter"
-par(mfrow=c(2,1))
-sapply(names(imgdat), function(x) hist(imgdat[[x]][,what], 100, col="blue", border="white", main=x, xlab=what))
-hin[["I"]] = as.data.frame(do.call(rbind, lapply(imgdat, function(x) x[,what,drop=F])))
-group = sapply(names(imgdat), function(x) rep(x, nrow(imgdat[[x]])))
-hin[["I"]]$CellLine = c(group[[1]], group[[2]])
-colnames(hin[["I"]])[1] = "Nucleus_Cell_AreaRatio" 
-hin[["I"]] = hin[["I"]][hin[["I"]][,1]>= quantile(hin[["I"]][,1],0.00),,drop=F]
-hin[["I"]] = hin[["I"]][hin[["I"]][,1]<= quantile(hin[["I"]][,1],1),,drop=F]
-easyGgplot2::ggplot2.histogram(data=hin[["I"]], xName = colnames(hin[["I"]])[1], groupName ="CellLine",alpha=0.5, addDensity=T,meanLineColor="white", bins=35, groupColors  = gray.colors(3)[1:2]) +    theme_minimal()
-
-## Sequencing data
-load("~/Projects/discussion/grantsAndAwards/Rita Allen 2019/Proposal/SelectionForces_GastricCLs/Results/listOfSeurats_G0G1_p.RData")
-seqdat = list(listOfSeurats$`NUGC-4`@assays$RNA@data, listOfSeurats$`KATOIII`@assays$RNA@data)
-names(seqdat) = fliplr(names(imgdat))
-
-cytnuc = lapply(seqdat, gatherNucleusCytoExpression, path2locmap, q1=0, q2=1)
-names(cytnuc) = names(seqdat)
-cytnuc = sapply(cytnuc, function(x) rbind(x$nucleus,x$cytosol))
-par(mfrow=c(2,1))
-sapply(names(cytnuc), function(x) hist(cytnuc[[x]][1,]/cytnuc[[x]][2,], 100, col="blue", border="white", main=x, xlim=c(0.55,0.75)))
-
-hin[["S"]] = as.data.frame(t(do.call(cbind,cytnuc)))
-group = sapply(names(cytnuc), function(x) rep(x, ncol(cytnuc[[x]])))
-hin[["S"]]$CellLine = c(group[[1]], group[[2]])
-colnames(hin[["S"]])[1:2] = c("Nucleus: Area","Cell: Area" )
-# colnames(hin[["S"]])[1:2] = c("Nucleus: Perimeter", "Cell: Perimeter")
-hin[["S"]]$Nucleus_Cytosol_ExpressionRatio = hin[["S"]]$`Nucleus: Area`/hin[["S"]]$`Cell: Area`
-easyGgplot2::ggplot2.histogram(data=hin[["S"]], xName = "Nucleus_Cytosol_ExpressionRatio", groupName ="CellLine",alpha=0.5, addDensity=T,meanLineColor="white", bins=35, groupColors  = gray.colors(3)[1:2]) +    theme_minimal()
-
-## Merge
-tmp = hin$S[,c("Nucleus_Cytosol_ExpressionRatio","CellLine")]
-tmp$Assay = "Sequencing"
-tmp2 = hin$I[,c("Nucleus_Cell_AreaRatio","CellLine")]
-tmp2$Nucleus_Cell_AreaRatio = log(tmp2$Nucleus_Cell_AreaRatio)
-tmp2$Nucleus_Cell_AreaRatio = tmp2$Nucleus_Cell_AreaRatio -min(tmp2$Nucleus_Cell_AreaRatio, na.rm=T)
-tmp2$Assay = "Imaging"
-tmp[,1] = tmp[,1]/mean(tmp[,1])
-tmp2[,1] = tmp2[,1]/mean(tmp2[,1])
-colnames(tmp) <- colnames(tmp2) <- c("Nucleus_Cytosol", "CellLine", "Assay") 
-tmp = rbind(tmp, tmp2)
-tmp = tmp[sort(tmp$Assay, index.return=T)$ix,]
-par(mai=c(1,3,1,1))
-boxplot(Nucleus_Cytosol ~ Assay + CellLine, data=tmp, col=gray.colors(2), horizontal=T, las=2, ylab="")
-legend("topleft", c("ScRNA-Seq", "Imaging"), fill=gray.colors(2))
-print(plyr::count(paste(tmp$CellLine,tmp$Assay)))
-
-
-## Co-cluster
-cytnucimg <- cytnucseq <- list()
-for(what in colnames(hin[["S"]])[1:2] ){
-  cytnucseq[[what]] = hin[["S"]][hin[["S"]]$CellLine=="SNU-16", what]
-  cytnucseq[[what]] = cytnucseq[[what]]- min(cytnucseq[[what]])
-  cytnucseq[[what]] = cytnucseq[[what]]/max(quantile(cytnucseq[[what]],0.995))
-  cytnucimg[[what]] = log(imgdat$`SNU-16`[,what])
-  cytnucimg[[what]] = cytnucimg[[what]]- quantile(cytnucimg[[what]],0.05)
-  cytnucimg[[what]] = cytnucimg[[what]]/max(quantile(cytnucimg[[what]],0.995))
-}
-par(mfrow=c(2,2))
-sapply(names(cytnucimg), function(x) hist(cytnucimg[[x]], xlab=x, main="img",xlim=c(0,1)))
-sapply(names(cytnucseq), function(x) hist(cytnucseq[[x]], xlab=x, main="seq",xlim=c(0,1)))
-
-## Sample cells from both assays
-N=50
-mm = rbind(cbind(cytnucseq[[1]],cytnucseq[[2]]),cbind(cytnucimg[[1]],cytnucimg[[2]]))
-rownames(mm) = c(rep("seq",length(cytnucseq[[1]])), rep("img",length(cytnucimg[[1]])))
-colnames(mm) = names(cytnucseq)
-ii = c(sample(grep("seq",rownames(mm)), N), sample(grep("img",rownames(mm)), N))
-mm = as.data.frame(mm[ii,])
-mm$ratio=mm[,1]/mm[,2]
-mm = mm[is.finite(mm$ratio),]
-## Tree
-dd = dist(mm[,2:3])
-tr = ape::nj(dd)
-col = rep("red", length(tr$tip.label))
-col[grep("img",tr$tip.label) ] = "blue"
-par(mfrow=c(1,1))
-plot(tr,show.tip.label = T, tip.color = col, cex=0.6)
-
-## How many sequenced cells match closer to an imaged cell than to a sequenced one:
-dd = as.matrix(dd)
-dd[dd==0]=NA
-rownames(dd) = sapply(strsplit(rownames(dd),".", fixed=T),"[[",1)
-colnames(dd) = sapply(strsplit(colnames(dd),".", fixed=T),"[[",1)
-bestmatch = paste(rownames(dd), colnames(dd)[apply(dd,1,which.min)])
-fr = plyr::count(bestmatch)
-rownames(fr) = fr$x
-print((fr["img seq",]$freq+fr["seq img",]$freq)/sum(fr$freq))
-
-
-
-
+# # align VAE output inside compartment
+# coord_nucl = coord[coord$nucleus==1,]
+# ## Clone_0.0027047_ID107807
+# vae = read.csv(paste0(ROOTD,"RNAsequencing/A02_210128_VAEoutput/identities_latentSpace3D.csv"))
+# vae=vae[vae$Location=="nucleus",]
+# vae=vae[!duplicated(vae),]
+# scatterplot3d::scatterplot3d(vae$x, vae$y, vae$z, pch=20)
+# alignPathways2Compartments(coord_nucl[,c("x","y","z")], vae[,c("x","y","z")])
 
