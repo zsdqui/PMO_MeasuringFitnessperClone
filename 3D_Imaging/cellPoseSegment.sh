e11a2f5c39edc5b2864344ad12ac6875e2548a26
noemiandor <noemi.andor@gmail.com>
2022-07-06 08:39:35 -0400

    optional argument to run segmentation only on a specific organelle

--- 3D_Imaging/cellPoseSegment.sh
+++ 3D_Imaging/cellPoseSegment.sh
@@ -1,21 +1,12 @@
-#!/bin/sh
-programname=$0
+#!/bin/bash
 
-usage() {
-  echo "usage: $programname FoF"
-  echo "  FoF:           unique FoF ID associated with this acquisition"
-  exit 1
-  }
-
-if [ $# -ne 4 ] ; then
-  usage
-fi
-
-## Command line arguments
 FoF=$1
 # FoF='FoF1_211027_fluorescent.mito'
+targetorganelle="all"
+if [ "$#" -eq 2 ]; then
+   targetorganelle=$2
+fi
 
-## Cellpose path, root path and target path
 run_CellPose='/raid/crdlab/ix1/Projects/M005_MeasuringFitnessPerClone_2019/code/run_CellPose_3D_v1.py'
 root='/raid/crdlab/ix1/Projects/M005_MeasuringFitnessPerClone_2019/data/GastricCancerCLs/3Dbrightfield/NCI-N87/'
 A03=$root/A03_allenModel/$FoF
@@ -31,6 +22,9 @@ for x in $f; do
   organelle="$(basename -- $x .tif)"
   organelle=${organelle%.*}
   organelle=`echo $organelle | sed 's/mito/mitochondria/g'`
+  if [[ $targetorganelle != "all" && $organelle != $targetorganelle ]]; then
+	continue;
+  fi
   # python3 cellPose_getCount.py -dir $x -t $organelle -GPU 1
   echo "running "$organelle" model on "$x
   python3 $run_CellPose -imgPath $x -t $organelle -GPU 1
@@ -44,3 +38,6 @@ done
 #mv $f $A04/All_Cells_coordinates/
 torm=`ls -d $A04/All_Cells_coordinates/*/`
 #rm -r $torm
+
+# Writing permissions to output FoF folder
+chmod -R a+w $A04
