e0cf25ff5e60137b1ad09cc01cdc31832aa1e2c7
noemiandor <noemi.andor@gmail.com>
2022-12-14 15:13:44 -0500

    post-submission big data service

--- 3D_Imaging/matlab/PseudoTime_RealTime_CrossCorr.m
+++ 3D_Imaging/matlab/PseudoTime_RealTime_CrossCorr.m
@@ -1,27 +1,30 @@
 cd ~/Projects/PMO/MeasuringFitnessPerClone/code/3D_Imaging/matlab/
 addpath cxcorr
-FoV='FoFX007_220523_brightfield';
+% FoV='FoFX007_220523_brightfield';
+FoV='FoFX001_220721_brightfield';
 csvf=['~/Projects/PMO/MeasuringFitnessPerClone/data/GastricCancerCL/3Dbrightfield/NCI-N87/A08_Pseudotime/',FoV,'.csv'];
 
 %% Read data
 csv=readtable(csvf);
-a=table2array(csv(:,'time_since_division'));
+%a=table2array(csv(:,'time_since_division'));
+a=table2array(csv(:,'frame'));
+%a=table2array(csv(:,'hour'));
 b=table2array(csv(:,'pseudotime'));
 % a=[1,2,3,4,5,6];
 % b=[7,8,3,4,5,6];
 
 [x,c]=cxcorr(a',b');
-c=max(c);
+[c,i]=max(c);
+i=i-2; %% 0 shift possible
+
 %% compare to cross correlation obtained from all random permuations of a
-A_=perms(a)';
-for a_=A_
+for j = 1:1000
+    a_=a(randperm(length(a)));
     [~,c_]=cxcorr(a_',b');
     c=[c,max(c_)];
 end
-[~,p]=ttest2(c(1),c(2:length(c)));
+[~,p]=ttest2(c(2:length(c)),c(1),'tail','left');
 
-[~,i]=max(c);
-i=i-2; %% 0 shift possible
 
 %% Shift data to maximize correlation
 b1=b((length(b)-i):length(b));
